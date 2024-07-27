10ba6cddda1d605dfa86b7666030b240eedfdc82
vmedej <70210745+vmedej@users.noreply.github.com>
2024-07-27 19:44:22 -0300

    Add files via upload

---
+++ js/text-area.js
@@ -0,0 +1,8 @@
+let textArea = document.querySelector("#texto-principal");
+textArea.addEventListener('input', autoResize, false);
+
+function autoResize() {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+}le.height = this.scrollHeight + 'px';
+}
